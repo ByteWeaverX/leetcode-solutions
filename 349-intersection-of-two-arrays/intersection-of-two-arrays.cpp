2c26f96fbf7b1fef897438c455a0e060a589d8da
Abhishek Dubey <abhishekdubey.smn@gmail.com>
2025-11-03 00:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (97.58%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -6,6 +6,7 @@ public:
         vector<int> ans;
         int i = 0, j = 0;
 
+
         while (i < nums1.size() && j < nums2.size()) {
             if (nums1[i] < nums2[j]) i++;
             else if (nums1[i] > nums2[j]) j++;
