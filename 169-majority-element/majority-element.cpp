6954b8066600581445d21e09ac55926041ef5a08
Abhishek Dubey <abhishekdubey.smn@gmail.com>
2025-11-03 00:27:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (28.85%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,7 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
         int count = 0;
         int candidate = 0;
-
         for (int num : nums) {
             if (count == 0) {
                 candidate = num;
