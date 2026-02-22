dc9d84aecc304d676a4e9acd54b882ec6f9e784c
Abhishek Dubey <abhishekdubey.smn@gmail.com>
2026-02-23 00:06:07 +0530

    Time: 5 ms (18.28%) | Memory: 16.4 MB (14.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,18 +1,18 @@
 class Solution {
 private:
-void solve(int open, int close, string current, vector<string>& ans,int n){
+void solve(int open, int close, int n, string current, vector<string>& ans){
     if(current.size()>=2*n){
         ans.push_back(current);
         return;
     }
     if(open<n){
         current.push_back('(');
-        solve(open+1, close, current, ans, n);
+        solve(open+1, close, n, current, ans);
         current.pop_back();
     }
     if(close<open){
         current.push_back(')');
-        solve(open, close+1, current, ans, n);
+        solve(open, close+1, n, current, ans);
         current.pop_back();
     }
 }
@@ -20,8 +20,8 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string current = "";
-        int open = 0 ; int close = 0;
-        solve(open, close, current, ans, n);
+        int open = 0 ; int close = 0 ; 
+        solve(open, close, n, current, ans);
         return ans;
     }
 };
